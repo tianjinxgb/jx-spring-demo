f966fdb05ef89ef93f7003dff9a34ac02d6e3904
xing.guobiao <tianjinxgb@126.com>
2020-10-24 18:47:18 +0800

    init jx spring demo

---
+++ src/test/java/com/xgb/test/jxspringdemo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgb.test.jxspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
